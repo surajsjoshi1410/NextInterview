55f66725164bc47a35087aea04336fc6de552534
thevasudev <vasudev082002@gmail.com>
2025-01-17 10:58:41 +0530

    m

--- src/modules/admin/pages/AddNewModule/AddNewModule.style.js
+++ src/modules/admin/pages/AddNewModule/AddNewModule.style.js
@@ -318,6 +318,7 @@ export const PaginationItem = styled.span`
     font-size: 0.625rem;
     margin-top: ${theme.spacing(2)};
     }
+    
 
   &:hover {
     background-color: ${theme.colors.sidebarHoverBgColor};
