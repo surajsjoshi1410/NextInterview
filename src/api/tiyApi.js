700fdb17857da7b06566efdddd821a2bba139177
jayanthbr29 <jayanthbr@digi9.co.in>
2025-02-22 12:08:50 +0530

    question mapping

---
+++ src/api/tiyApi.js
@@ -0,0 +1,41 @@
+import api from "../config/axiosconfig";    
+
+
+export const gettiy = async (module_code, topic_code, subtopic_code, question_type,level) => {
+    try {
+      const params = {};
+  
+      if (module_code) {
+        params.module_code = module_code;
+      }
+      if (topic_code) {
+        params.topic_code = topic_code;
+      }
+      if (subtopic_code) {
+        params.subtopic_code = subtopic_code;
+      }
+      if (question_type) {
+        params.question_type = question_type;
+      }
+      if (level) {
+        params.level = level;
+      }
+  
+      const response = await api.get('/tiy/get', { params });
+  
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  };
+
+  export const gettiyById = async (id) => {
+    try {
+      const response = await api.get(`/tiy/get/${id}`);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  };
